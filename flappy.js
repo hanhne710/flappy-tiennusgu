ba56738ddfc77b4eee78a164bb5baff5fb8c036d
hanhne710 <buihanh07102001@gmail.com>
2024-02-22 16:55:28 +0700

    u điện thoại

--- flappy.js
+++ flappy.js
@@ -1,158 +1,162 @@
 const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d");
 
-const gameContainer = document.getElementById('container');
+        const gameContainer = document.getElementById('container');
 
-const tienNuIMG = new Image();
-tienNuIMG.src = './assets/assets/tien-nu-sgu.png';
+        const tienNuIMG = new Image();
+        tienNuIMG.src = './assets/assets/tien-nu-sgu.png';
 
-const FLAP_SPEED = -3;
-const BIRD_WIDTH = 40;
-const BIRD_HEIGHT = 30;
-const PIPE_WIDTH = 50;
-const PIPE_GAP = 105;
+        const FLAP_SPEED = -3;
+        const BIRD_WIDTH = 40;
+        const BIRD_HEIGHT = 30;
+        const PIPE_WIDTH = 50;
+        const PIPE_GAP = 105;
 
-let birdX = 50;
-let birdY = 50;
-let birdVelocity = 0;
-let birdAcceleration = 0.05;
+        let birdX = 50;
+        let birdY = 50;
+        let birdVelocity = 0;
+        let birdAcceleration = 0.05;
 
-let pipeX = 400;
-let pipeY = canvas.height - 200;
+        let pipeX = 400;
+        let pipeY = canvas.height - 200;
 
-let scoreDiv = document.getElementById('score-display');
-let score = 0;
-let highScore = 0;
+        let scoreDiv = document.getElementById('score-display');
+        let score = 0;
+        let highScore = 0;
 
-let scored = false;
+        let scored = false;
 
-document.body.onkeyup = function(e) {
-    if (e.code == 'Space') {
-        birdVelocity = FLAP_SPEED;
-    }
-}
+        document.body.addEventListener('touchstart', function() {
+            birdVelocity = FLAP_SPEED;
+        });
 
-document.getElementById('restart-button').addEventListener('click', function() {
-    hideEndMenu();
-    resetGame();
-    loop();
-})
+        document.body.addEventListener('keydown', function(e) {
+            if (e.code == 'Space') {
+                birdVelocity = FLAP_SPEED;
+            }
+        });
 
-function increaseScore() {
-    if(birdX > pipeX + PIPE_WIDTH && 
-        (birdY < pipeY + PIPE_GAP || 
-          birdY + BIRD_HEIGHT > pipeY + PIPE_GAP) && 
-          !scored) {
-        score++;
-        scoreDiv.innerHTML = score;
-        scored = true;
-    }
+        document.getElementById('restart-button').addEventListener('click', function() {
+            hideEndMenu();
+            resetGame();
+            loop();
+        })
 
-    if (birdX < pipeX + PIPE_WIDTH) {
-        scored = false;
-    }
-}
+        function increaseScore() {
+            if(birdX > pipeX + PIPE_WIDTH && 
+                (birdY < pipeY + PIPE_GAP || 
+                birdY + BIRD_HEIGHT > pipeY + PIPE_GAP) && 
+                !scored) {
+                score++;
+                scoreDiv.innerHTML = score;
+                scored = true;
+            }
 
-function collisionCheck() {
-    const birdBox = {
-        x: birdX,
-        y: birdY,
-        width: BIRD_WIDTH,
-        height: BIRD_HEIGHT
-    }
+            if (birdX < pipeX + PIPE_WIDTH) {
+                scored = false;
+            }
+        }
 
-    const topPipeBox = {
-        x: pipeX,
-        y: pipeY - PIPE_GAP + BIRD_HEIGHT,
-        width: PIPE_WIDTH,
-        height: pipeY
-    }
+        function collisionCheck() {
+            const birdBox = {
+                x: birdX,
+                y: birdY,
+                width: BIRD_WIDTH,
+                height: BIRD_HEIGHT
+            }
 
-    const bottomPipeBox = {
-        x: pipeX,
-        y: pipeY + PIPE_GAP + BIRD_HEIGHT,
-        width: PIPE_WIDTH,
-        height: canvas.height - pipeY - PIPE_GAP
-    }
+            const topPipeBox = {
+                x: pipeX,
+                y: pipeY - PIPE_GAP + BIRD_HEIGHT,
+                width: PIPE_WIDTH,
+                height: pipeY
+            }
 
-    if (birdBox.x + birdBox.width > topPipeBox.x &&
-        birdBox.x < topPipeBox.x + topPipeBox.width &&
-        birdBox.y < topPipeBox.y) {
-            return true;
-    }
+            const bottomPipeBox = {
+                x: pipeX,
+                y: pipeY + PIPE_GAP + BIRD_HEIGHT,
+                width: PIPE_WIDTH,
+                height: canvas.height - pipeY - PIPE_GAP
+            }
 
-    if (birdBox.x + birdBox.width > bottomPipeBox.x &&
-        birdBox.x < bottomPipeBox.x + bottomPipeBox.width &&
-        birdBox.y + birdBox.height > bottomPipeBox.y) {
-            return true;
-    }
+            if (birdBox.x + birdBox.width > topPipeBox.x &&
+                birdBox.x < topPipeBox.x + topPipeBox.width &&
+                birdBox.y < topPipeBox.y) {
+                    return true;
+            }
 
-    if (birdY < 0 || birdY + BIRD_HEIGHT > canvas.height) {
-        return true;
-    }
+            if (birdBox.x + birdBox.width > bottomPipeBox.x &&
+                birdBox.x < bottomPipeBox.x + bottomPipeBox.width &&
+                birdBox.y + birdBox.height > bottomPipeBox.y) {
+                    return true;
+            }
+
+            if (birdY < 0 || birdY + BIRD_HEIGHT > canvas.height) {
+                return true;
+            }
 
 
-    return false;
-}
+            return false;
+        }
 
-function hideEndMenu () {
-    document.getElementById('end').style.display = 'none';
-    gameContainer.classList.remove('backdrop-blur');
-}
+        function hideEndMenu () {
+            document.getElementById('end').style.display = 'none';
+            gameContainer.classList.remove('backdrop-blur');
+        }
 
-function showEndMenu () {
-    document.getElementById('end').style.display = 'block';
-    gameContainer.classList.add('backdrop-blur');
-    document.getElementById('end-score').innerHTML = score;
-    if (highScore < score) {
-        highScore = score;
-    }
-    document.getElementById('best-score').innerHTML = highScore;
-}
+        function showEndMenu () {
+            document.getElementById('end').style.display = 'block';
+            gameContainer.classList.add('backdrop-blur');
+            document.getElementById('end-score').innerHTML = score;
+            if (highScore < score) {
+                highScore = score;
+            }
+            document.getElementById('best-score').innerHTML = highScore;
+        }
 
-function resetGame() {
-    birdX = 50;
-    birdY = 50;
-    birdVelocity = 0;
-    birdAcceleration = 0.1;
+        function resetGame() {
+            birdX = 50;
+            birdY = 50;
+            birdVelocity = 0;
+            birdAcceleration = 0.1;
 
-    pipeX = 400;
-    pipeY = canvas.height - 200;
+            pipeX = 400;
+            pipeY = canvas.height - 200;
 
-    score = 0;
-}
+            score = 0;
+        }
 
-function endGame() {
-    showEndMenu();
-}
+        function endGame() {
+            showEndMenu();
+        }
 
-function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        function loop() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw Flappy Bird
-    ctx.drawImage(tienNuIMG, birdX, birdY);
+            // Draw Flappy Bird
+            ctx.drawImage(tienNuIMG, birdX, birdY);
 
-    ctx.fillStyle = '#D350D6';
-    ctx.fillRect(pipeX, -100, PIPE_WIDTH, pipeY);
-    ctx.fillRect(pipeX, pipeY + PIPE_GAP, PIPE_WIDTH, canvas.height - pipeY);
+            ctx.fillStyle = '#D350D6';
+            ctx.fillRect(pipeX, -100, PIPE_WIDTH, pipeY);
+            ctx.fillRect(pipeX, pipeY + PIPE_GAP, PIPE_WIDTH, canvas.height - pipeY);
 
-    if (collisionCheck()) {
-        endGame();
-        return;
-    }
+            if (collisionCheck()) {
+                endGame();
+                return;
+            }
 
-    pipeX -= 1.5;
+            pipeX -= 1.5;
 
-    if (pipeX < -50) {
-        pipeX = 400;
-        pipeY = Math.random() * (canvas.height - PIPE_GAP) + PIPE_WIDTH;
-    }
+            if (pipeX < -50) {
+                pipeX = 400;
+                pipeY = Math.random() * (canvas.height - PIPE_GAP) + PIPE_WIDTH;
+            }
 
-    birdVelocity += birdAcceleration;
-    birdY += birdVelocity;
+            birdVelocity += birdAcceleration;
+            birdY += birdVelocity;
 
-    increaseScore()
-    requestAnimationFrame(loop);
-}
+            increaseScore()
+            requestAnimationFrame(loop);
+        }
 
-loop();
+        loop();
